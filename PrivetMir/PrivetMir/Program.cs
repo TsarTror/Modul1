0c24d145f90b9cc1361b9b9b061c82c9a3db7d80
TsarTror <132853183+TsarTror@users.noreply.github.com>
2023-05-15 19:19:49 +0700

    Add files via upload

---
+++ PrivetMir/PrivetMir/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivetMir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет,");  // Вывод текста
+                                                // в консоль
+
+           
+            Console.Write(" мир");  // Вывод текста
+                                                // в консоль
+
+            
+            Console.Write("!!!");  // Вывод текста
+                                              
+
+            Console.ReadKey();                  // Ожидание от пользователя 
+                                                // нажатия какой-либокнопки 
+
+        }
+    }
+}
