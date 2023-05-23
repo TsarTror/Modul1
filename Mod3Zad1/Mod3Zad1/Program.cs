2e226f6258d68be7bb0837998d96a8d56dff62b1
TsarTror <132853183+TsarTror@users.noreply.github.com>
2023-05-24 05:42:03 +0700

    Add files via upload

---
+++ Mod3Zad1/Mod3Zad1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3Zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Введите число:");
+            int a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Число чётное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечётное");
+            }
+            Console.ReadKey();
+        }
+    }
+}
